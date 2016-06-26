515f5685f53ea4c2d30fb67a81037e581d108a11
cs982005 <h_alzainy@hotmail.com>
2016-06-26 17:06:07 -0400

    testing sys.argv

--- m4a-grab.py
+++ m4a-grab.py
@@ -1,5 +1,7 @@
-wget $1
+#!/usr/bin/python
 
-# <td width="12%"><div align="center"><a href="http://www.melody4arab.com/download/ar_download_105264.htm"><img src="../images/s2.jpg" height="14" width="14" alt="تحميل اغنية فاض بيا" border="0"></a></div></td>
-
+import sys
+print "This is the name of the script: ", sys.argv[0]
+print "Number of arguments: ", len(sys.argv)
+print "The arguments are: " , str(sys.argv)
 
